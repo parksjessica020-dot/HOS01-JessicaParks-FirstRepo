b30199a4e07a6f5a67d95a33285bba1a83db670b
parksjessica020-dot <parksjessica@cityuniversity.edu>
2026-04-06 21:58:21 +0000

    HOS01-Jessica Parks

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+class HelloWorld:
+
+    def __init__(self):
+        self.message = "Hello, World!"
+
+    def display(self):   
+        print(self.message)
+        
+greeting = HelloWorld()
+greeting.display()
